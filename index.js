84838ad6ad66531eae5c9dad4a62504285ab9588
Shahjalal Hazari <shahjalalhazari1@gmail.com>
2023-06-09 11:53:18 +0400

    Getting cart item by user email for each user.

--- index.js
+++ index.js
@@ -55,6 +55,16 @@ async function run() {
 
 
         // Cart Collections
+        app.get("/carts", async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([])
+            }
+            const query = {email: email};
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        })
+
         app.post("/carts", async (req, res) => {
             const item = req.body;
             console.log(item);
